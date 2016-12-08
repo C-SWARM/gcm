8acdea4a7489cb9afe35cf0ebc7893f4b0af2d29
Sangmin Lee <slee43@nd.edu>
2016-12-08 12:45:49 -0500

    add more comments and function declaration (staggered_Newton_Rapson_generalized)

--- crystal_plasticity/include/crystal_plasticity_integration.h
+++ crystal_plasticity/include/crystal_plasticity_integration.h
@@ -24,7 +24,46 @@ int set_crystal_plasticity_solver_info(CRYSTAL_PLASTICITY_SOLVER_INFO *solver_in
 
 int print_crystal_plasticity_solver_info(CRYSTAL_PLASTICITY_SOLVER_INFO *solver_info);     
 
-int staggered_Newton_Rapson(double *pFnp1_out,double *M_out, double *g_out, double *lambda, 
+/// integrate crystal plasticity from pFn to pFnp1 for general cases (includes thermal expansions)
+///
+/// \param[out] pFnp1_out deformation gradient (plastic) at time step = n + 1
+/// \param[out] M_out deformation gradient (pFr_I) at time step = n + 1
+/// \param[out] g_out updated hardening at time step =  n + 1
+/// \param[out] lambda updated Lagrange multiplier at time step =  n + 1
+/// \param[in] pFn_in deformation gradient (plastic) at time step = n
+/// \param[in] Fn_in deformation gradient (total) at time step = n
+/// \param[in] Fnp1_in deformation gradient (total) at time step = n + 1
+/// \param[in] hFn_in deformation gradient (due to thermal expansion) at time step = n
+/// \param[in] hFnp1_in deformation gradient (due to thermal expansion) at time step = n + 1
+/// \param[in] g_n hardening at time step =  n
+/// \param[in] dt time step size
+/// \param[in] mat material parameters
+/// \param[in] elasticity object to compute elasticity (stress)
+/// \param[in] solver_info defines numerical parameters 
+/// \return non-zero on interal error
+int staggered_Newton_Rapson_generalized(double *pFnp1_out, double *M_out, double *g_out, double *lambda, 
+                                        double *pFn_in, double *Fn_in, double *Fnp1_in, double *hFn_in, double *hFnp1_in,  
+                                        double g_n, double dt, 
+                                        MATERIAL_CONSTITUTIVE_MODEL *mat,
+                                        ELASTICITY *elasticity, 
+                                        CRYSTAL_PLASTICITY_SOLVER_INFO *solver_info);
+
+/// integrate crystal plasticity from pFn to pFnp1
+///
+/// \param[out] pFnp1_out deformation gradient (plastic) at time step = n + 1
+/// \param[out] M_out deformation gradient (pFr_I) at time step = n + 1
+/// \param[out] g_out updated hardening at time step =  n + 1
+/// \param[out] lambda updated Lagrange multiplier at time step =  n + 1
+/// \param[in] pFn_in deformation gradient (plastic) at time step = n
+/// \param[in] Fn_in deformation gradient (total) at time step = n
+/// \param[in] Fnp1_in deformation gradient (total) at time step = n + 1
+/// \param[in] g_n hardening at time step =  n
+/// \param[in] dt time step size
+/// \param[in] mat material parameters
+/// \param[in] elasticity object to compute elasticity (stress)
+/// \param[in] solver_info defines numerical parameters 
+/// \return non-zero on interal error 
+int staggered_Newton_Rapson(double *pFnp1_out, double *M_out, double *g_out, double *lambda, 
                             double *pFn_in, double *Fn_in, double *Fnp1_in, 
                             double g_n, double dt, 
                             MATERIAL_CONSTITUTIVE_MODEL *mat,
