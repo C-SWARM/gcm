446a8d3f0e9a64c47795677d103ca7c05c5725f0
Sangmin Lee <slee43@nd.edu>
2017-05-03 14:51:42 -0400

    constitutive model interface is created to use PVP model in PGFem3D

---
+++ poro_viscoplasticity/include/pvp_interface.h
@@ -0,0 +1,48 @@
+
+#ifndef H_PVP_INTERFACE_H
+#define H_PVP_INTERFACE_H
+
+#include "KMS-IJSS2017.h"
+
+
+
+int pvp_intf_perform_integration_algorithm2(double *Fnp1,
+                                           double *Fn,
+                                           double *pFnp1,
+                                           double *pFn,
+                                           double *pc_np1,
+                                           double pc_n,
+                                           KMS_IJSS2017_Parameters *mat_pvp,
+                                           double dt);
+
+int pvp_intf_perform_integration_algorithm(double *Fnp1,
+                                           double *Fn,
+                                           double *pFnp1,
+                                           double *pFn,
+                                           double *pc_np1,
+                                           double pc_n,
+                                           KMS_IJSS2017_Parameters *mat_pvp,
+                                           double dt);
+
+double pvp_intf_hardening_law(double pc,
+                              KMS_IJSS2017_Parameters *mat_pvp);                                           
+                                           
+int pvp_intf_update_elasticity(double *eF,
+                               double pc,
+                               double *S_in,
+                               double *L_in,
+                               KMS_IJSS2017_Parameters *mat_pvp,
+                               int compute_stiffness);
+
+int pvp_intf_compute_dMdF(double *dMdF_in,
+                          double *Fnp1,
+                          double *Fn,
+                          double *pFnp1,
+                          double *pFn,
+                          double pc_np1,
+                          double pc_n,
+                          KMS_IJSS2017_Parameters *mat_pvp,
+                          double dt);
+
+#endif
+
