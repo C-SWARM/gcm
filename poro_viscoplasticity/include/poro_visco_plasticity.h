16cceac1bd77a82372059301fbfe43aa964384b3
Sangmin Lee <slee43@nd.edu>
2018-07-05 14:13:57 -0400

    remove old pvp model

--- poro_viscoplasticity/include/poro_visco_plasticity.h
+++ poro_viscoplasticity/include/poro_visco_plasticity.h
@@ -28,5 +28,44 @@ void poro_visco_plasticity_update_elasticity(double *eS_out,
 
 double poro_visco_plasticity_hardening(const double pc,
                                        const MaterialPoroViscoPlasticity *param);
+                                       
+/// compute conforming pressure for given plastic deformation 
+/// \param[in] pJ        determinant of pF
+/// \param[in] mat_pvp   poro_viscoplaticity material object
+/// \return    computed pc value  
+double poro_visco_plasticity_compute_pc(double pJ, 
+                                        double pc,
+                                        const MaterialPoroViscoPlasticity *mat,
+                                        const GcmSolverInfo *solver_info);
+
+void poro_visco_plasticity_update_elasticity_dev(double *eS_out,
+                                                 double *L_out,
+                                                 const MaterialPoroViscoPlasticity *param,
+                                                 double *eF_in,
+                                                 const double pc,
+                                                 const bool compute_4th_order);
+
+double poro_visco_plasticity_intf_compute_dudj(const double eJ,
+                                               const double pc,
+                                               const MaterialPoroViscoPlasticity *param);                                               
+
+double poro_visco_plasticity_intf_compute_d2udj2(const double eJ,
+                                                 const double pc,
+                                                 const MaterialPoroViscoPlasticity *param);                                                 
+
+double poro_visco_plasticity_hardening(const double pc,
+                                       const MaterialPoroViscoPlasticity *param);
+
+void poro_visco_plasticity_intf_compute_gammas(double &gamma_dot_d,
+                                               double &gamma_dot_v,
+                                               double *Fnp1_in,
+                                               double *Fn_in,
+                                               double *pFnp1_in,
+                                               double *pFn_in,
+                                               const double pc_np1,
+                                               const double pc_n,
+                                               const double dt,
+                                               const MaterialPoroViscoPlasticity *param,
+                                               const GcmSolverInfo *solver_info);
                                              
 # endif
